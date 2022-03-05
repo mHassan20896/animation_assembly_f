ebf757fc557068a8f60e0d04c4da843424232a2d
Hassan <hassan.shakeel@venturedive.com>
2022-03-05 17:10:11 +0500

    small optimization

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -2,14 +2,9 @@ import 'package:animation_demo/detailed_page.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
-class MainPage extends StatefulWidget {
+class MainPage extends StatelessWidget {
   const MainPage({Key? key}) : super(key: key);
 
-  @override
-  State<MainPage> createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
